76b3e0c54697d491b32fc7d3dfdcee99fc790251
Alejandro <alejbarros@gmail.com>
2018-08-21 22:33:02 -0700

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Alejandro Barros Fuentes on 8/21/18.
+//  Copyright © 2018 Alejandro Barros Fuentes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
